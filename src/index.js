9e79731cfcd30c3d917ece1af4d953ea0d15275e
Robert creativo <ronygarena3@gmail.com>
2026-09-06 12:42:38 -0500

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app");
-
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`✅ Servidor de correo escuchando en http://localhost:${PORT}`);
-});
- 
